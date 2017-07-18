1e47d0fe6585c1bad64c203795272f12b8faf84d
kden <skishida@users.noreply.github.com>
2017-07-18 13:58:08 +0900

    Create ex4.scm

---
+++ ex4.scm
@@ -0,0 +1,35 @@
+(define-syntax stream-cons
+    (syntax-rules ()
+        ((_ x y) (cons x (delay y)))
+    ))
+
+(define-syntax stream-car
+    (syntax-rules ()
+        ((_ x) (car x ))
+    ))
+
+(define-syntax stream-cdr
+    (syntax-rules ()
+        ((_ x) (force (cdr x)))
+    ))
+
+(define numbers (lambda ()
+  (letrec ((stream
+              (lambda (n) (stream-cons n (stream (+ n 1))))
+          ))
+          (stream 2))))
+
+(define head (lambda (n L)
+  (if (<= n 0) '()
+      (cons (stream-car L) (head (- n 1) (stream-cdr L)))
+      )))
+
+(define sieve (lambda (num L)
+  (if (equal? (modulo (stream-car L) num) '0) (sieve num (stream-cdr L))
+      (stream-cons (stream-car L) (sieve num (stream-cdr L)))
+      )))
+(define primes (lambda (n L)
+))
+
+(head 5 (numbers))
+(head 10 (sieve 2 (numbers)))
